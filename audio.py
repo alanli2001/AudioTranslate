5267765ad85a321834f34faa635b03f6a117bee5
alanli2001 <alanli2001@users.noreply.github.com>
2020-10-03 15:19:53 -0400

    idk

--- audio.py
+++ audio.py
@@ -48,5 +48,5 @@ while True:
             print("Google Speech Recognition could not understand audio")
         except sr.RequestError as e:
             print("Could not request results from Google Speech Recognition service; {0}".format(e))
-#connor sux
+#connor sux 
 
