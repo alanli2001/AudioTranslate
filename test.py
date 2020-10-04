2232a16c95476502025c93b72dd99249f4a40315
alanli2001 <alanli2001@users.noreply.github.com>
2020-10-04 00:17:59 -0400

    added oauth

--- test.py
+++ test.py
@@ -18,6 +18,28 @@ from google.cloud import speech
 import pyaudio
 from six.moves import queue
 
+from google_auth_oauthlib import flow
+
+# TODO: Uncomment the line below to set the `launch_browser` variable.
+launch_browser = True
+#
+# The `launch_browser` boolean variable indicates if a local server is used
+# as the callback URL in the auth flow. A value of `True` is recommended,
+# but a local server does not work if accessing the application remotely,
+# such as over SSH or from a remote Jupyter notebook.
+
+appflow = flow.InstalledAppFlow.from_client_secrets_file(
+    "client_secrets.json", scopes=["https://www.googleapis.com/auth/bigquery"]
+)
+
+if launch_browser:
+    appflow.run_local_server()
+else:
+    appflow.run_console()
+
+credentials = appflow.credentials
+
+
 # Audio recording parameters
 STREAMING_LIMIT = 240000  # 4 minutes
 SAMPLE_RATE = 16000
