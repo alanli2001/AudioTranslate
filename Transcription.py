e18f72d14b7a324e7b7acbc8924dc15d097ac0c1
alanli2001 <alanli2001@users.noreply.github.com>
2020-10-04 04:31:21 -0400

    add radio buttons, incorporated transcription

--- Transcription.py
+++ Transcription.py
@@ -1,9 +1,4 @@
-'''
-A simple Gooey example. One required field, one optional.
-'''
-
 from gooey import Gooey, GooeyParser
-
 import test
 
 
@@ -11,13 +6,40 @@ import test
 def main():
     parser = GooeyParser(description='Transcribe/translate audio.')
 
-    parser.add_argument(
-        'required_field',
-        metavar='Language',
-        help='Choose the language for the output text in the drop-down menu.')
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("-en", "--English", action="store_true")
+    group.add_argument("-hi", "--Hindi", action="store_true")
+    group.add_argument("-es", "--Spanish", action="store_true")
+    group.add_argument("-fr", "--French", action="store_true")
+    group.add_argument("-ar", "--Arabic", action="store_true")
+    group.add_argument("-bn", "--Bengali", action="store_true")
+    group.add_argument("-ru", "--Russian", action="store_true")
+    group.add_argument("-pt", "--Portuguese", action="store_true")
+    group.add_argument("-id", "--Indonesian", action="store_true")
+    args = parser.parse_args()
 
-    parser.parse_args()
-    test.main()
+    def run():
+        if args.English:
+            test.main("en")
+        elif args.Hindi:
+            test.main("hi")
+        elif args.Spanish:
+            test.main("es")
+        elif args.French:
+            test.main("fr")
+        elif args.Arabic:
+            test.main("ar")
+        elif args.Bengali:
+            test.main("bn")
+        elif args.Russian:
+            test.main("ru")
+        elif args.Portuguese:
+            test.main("pt")
+        elif args.Indonesian:
+            test.main("id")
+        else:
+            test.main("en")
+    run()
     print('Transcription complete!')
 
 
